b4295097ca3724aa0a718305c9ad2d53e0728a8d
Samuel Holmes <samuelHdev@outlook.com>
2023-09-30 12:03:24 -0400

    login done -- add styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/login';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,8 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
 export default function LoginPage() {
 
 
@@ -6,16 +11,34 @@ export default function LoginPage() {
   }
 
   return (
-    <>
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={login}>
-          <input type="text" name="email" />
-          <input type="text" name="password" />
-          <button type="submit">Login</button>
-        </form>
+    <div className='bg-light'>
+      <div className="d-flex flex-row flex-wrap justify-content-center">
+       
 
+        <Form>
+        <h2 className='d-flex justify-content-center'>Login</h2>
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="email" placeholder="Email" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+        <Form.Label column sm={2}>
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="password" placeholder="Password" />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button type="submit">Sign in</Button>
+        </Col>
+      </Form.Group>
+      </Form>
       </div>
-    </>
+    </div>
   )
 }
