f0007c1fb4437670936fb16cf32448fcdbf87067
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-09-30 12:23:37 -0400

    added search logic for table

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -1,27 +1,48 @@
+import { useEffect, useState } from 'react';
 import '../styles/employeeTable.css'
 
 export default function EmployeeTable({ employeeList }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
     return (
-      <div className="d-flex justify-content-center">
-        <table className="table employeeTable mt-5">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>First Name</td>
-              <td>Last Name</td>
-              <td>(123) 456-789</td>
-              <td><a href='/:employee'>Details</a></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <>      
+        <div className="d-flex justify-content-center">
+          <div className="">
+            <input type='text' placeholder='Search Employee' onChange={(e) => setSearchQuery(e.target.value)}/>
+            <table className="table employeeTable mt-5">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">First</th>
+                  <th scope="col">Last</th>
+                  <th scope="col">Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                {searchQuery === "" 
+                  ? (employeeList.map((employee, index) => (
+                    <tr key={index}>
+                      <th scope="row">{employee.id}</th>
+                      <td>{employee.firstName}</td>
+                      <td>{employee.lastName}</td>
+                      <td>{employee.phone}</td>
+                      <td><a href='/:employee'>Details</a></td>
+                    </tr>
+                  )))
+                  : (employeeList.filter(employee => employee.firstName.toLowerCase().includes(searchQuery.toLowerCase())).map((employee, index) => (
+                    <tr key={index}>
+                      <th scope="row">{employee.id}</th>
+                      <td>{employee.firstName}</td>
+                      <td>{employee.lastName}</td>
+                      <td>{employee.phone}</td>
+                      <td><a href='/:employee'>Details</a></td>
+                    </tr>
+                  )))
+                  }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
     );
 }
