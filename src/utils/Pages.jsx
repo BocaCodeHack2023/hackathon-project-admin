38b36c8e7000ce7fcc3fdb81f47bdcc7fe57ffcf
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-10-01 04:50:16 -0400

    set up route for employees page

--- src/utils/Pages.jsx
+++ src/utils/Pages.jsx
@@ -2,5 +2,6 @@ import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login.jsx";
 import EmployeeProfile from "../pages/EmployeeProfile";
 import Reports from "../pages/Reports";
+import Employees from "../pages/Employees";
 
-export { Dashboard, Login, EmployeeProfile, Reports }
+export { Dashboard, Login, EmployeeProfile, Reports, Employees }
